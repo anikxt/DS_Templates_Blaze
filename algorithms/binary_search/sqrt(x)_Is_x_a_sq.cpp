921ab3a3f14526d023448674d0603f467f5ab38e
Aniket Kanere <aniket15304@gmail.com>
2021-09-03 13:56:02 +0530

    Updated

--- algorithms/binary_search/sqrt(x)_Is_x_a_sq.cpp
+++ algorithms/binary_search/sqrt(x)_Is_x_a_sq.cpp
@@ -1,30 +1,3 @@
-#pragma GCC optimize "trapv"
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("O3", "unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-#include <bits/stdc++.h>
-using namespace std;
-#define gc getchar_unlocked
-#define fab(a, b, i) for (int i = a; i < b; ++i)
-#define deb(x) cout << #x << "=" << x << endl
-#define pb push_back
-#define mp make_pair
-#define f first
-#define se second
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define endl '\n'
-#define blaze ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-typedef long long int ll;
-//=======================
-const int N = 2e6 + 13, M = N;
-//=======================
-int a[N];
-int c, k, m, n, t, x, y;
-//=======================
-
 // sqrt(x) - Is x a perfect square?
 
 int solve()
@@ -48,11 +21,4 @@ int solve()
     }
   }
   return -1;
-}
-
-int main()
-{
-  blaze;
-  cout << solve() << endl;
-  return 0;
 }
