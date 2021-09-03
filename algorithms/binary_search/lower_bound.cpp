c3dc3214ebacc3fb0095751f61a2615f18d757ec
Aniket Kanere <aniket15304@gmail.com>
2021-09-03 14:00:18 +0530

    Updated

--- algorithms/binary_search/lower_bound.cpp
+++ algorithms/binary_search/lower_bound.cpp
@@ -2,16 +2,13 @@
 
 int solve()
 {
-  cin >> n;
-  int a[n];
-  fab(0, n, i) { cin >> a[i]; }
-  cin >> t;
-  sort(a, a + n);
+  int a[n]; // sorted
+  int x; // target element
   int low = 0, high = n - 1, res = -1;
   while (low <= high)
   {
     int mid = low + (high - low) / 2;
-    if (a[mid] >= t)
+    if (a[mid] >= x)
     {
       res = mid;
       high = mid - 1;
