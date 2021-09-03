2999db8283d7b3e1f38c7e6f0219f7923784e0de
Aniket Kanere <aniket15304@gmail.com>
2021-09-03 13:57:22 +0530

    Updated

--- algorithms/binary_search/lower_bound.cpp
+++ algorithms/binary_search/lower_bound.cpp
@@ -1,30 +1,3 @@
-#pragma GCC optimize "trapv"
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("O3", "unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-#include <bits/stdc++.h>
-using namespace std;
-#define gc getchar_unlocked
-#define fab(a, b, i) for (int i = a; i < b; ++i)
-#define deb(x) cout << #x << "=" << x << endl
-#define pb push_back
-#define mp make_pair
-#define f first
-#define se second
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define endl '\n'
-#define blaze ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-typedef long long int ll;
-//=======================
-const int N = 2e6 + 13, M = N;
-//=======================
-int a[N];
-int c, k, m, n, t, x, y;
-//=======================
-
 // lower bound - Find first value >= x
 
 int solve()
@@ -49,11 +22,4 @@ int solve()
     }
   }
   return res;
-}
-
-int main()
-{
-  blaze;
-  cout << solve() << endl;
-  return 0;
 }
