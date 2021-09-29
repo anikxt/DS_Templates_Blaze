6fc68f5fd713ebccfd92bd980140dca76228383b
Aniket Kanere <aniket15304@gmail.com>
2021-09-29 18:52:56 +0530

    Updated

--- algorithms/amortized/kadane's_algorithm/Kadane's_Algo.cpp
+++ algorithms/amortized/kadane's_algorithm/Kadane's_Algo.cpp
@@ -17,7 +17,7 @@ public:
     int best = INT_MIN, sum = 0;
     for (int i = 0; i < nums.size(); ++i)
     {
-      sum = max(nums[i], sum + nums[i]);
+      sum = max(sum + nums[i], nums[i]);
       best = max(best, sum);
     }
     return best;
